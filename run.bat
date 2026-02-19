52ea8af4fc6a6061a9ab4e9a675244ceafa713aa
Dabuek Chrobak <danielchrobak12@gmail.com>
2026-02-18 19:42:49 -0600

    Remove run.bat

--- run.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-net session >nul 2>&1 || (powershell -Command "Start-Process -FilePath '%~f0' -Verb RunAs" & exit /b)
-cd /d "%~dp0"
-if not exist "build\bin\Release\SlipStream.exe" (echo Run build.bat first & pause & exit /b 1)
-cd build\bin\Release && SlipStream.exe
-echo.
-echo [SlipStream exited with code %errorlevel%]
-pause
